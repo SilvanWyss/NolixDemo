3f85c75a6616976fef5cc208b7e942a225bccfaa
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-23 16:30:02 +0200

    switched HelloWorldApplicationDemo to web GUI

--- Source/ch/nolix/nolixdemo/helloworldapplication/HelloWorldApplicationDemo.java
+++ Source/ch/nolix/nolixdemo/helloworldapplication/HelloWorldApplicationDemo.java
@@ -1,24 +1,23 @@
 package ch.nolix.nolixdemo.helloworldapplication;
 
-import ch.nolix.system.application.guiapplication.BackendGUIClient;
-import ch.nolix.system.application.guiapplication.BackendGUIClientSession;
 import ch.nolix.system.application.main.Application;
 import ch.nolix.system.application.main.Server;
 import ch.nolix.system.application.main.VoidApplicationContext;
-import ch.nolix.system.gui.widget.Label;
-import ch.nolix.systemapi.guiapi.widgetguiapi.ControlState;
+import ch.nolix.system.application.webapplication.BackendWebClient;
+import ch.nolix.system.application.webapplication.BackendWebClientSession;
+import ch.nolix.system.webgui.control.Text;
+import ch.nolix.systemapi.webguiapi.mainapi.ControlState;
 
 public final class HelloWorldApplicationDemo {
 	
 	public static void main(String[] args) {
 		
-		final var server = Server.forDefaultPort();
+		final var server = Server.forDefaultPort(true);
 		
 		server.addDefaultApplication(new HelloWorldApplication());
 	}
 	
-	public static final class HelloWorldApplication
-	extends Application<BackendGUIClient<VoidApplicationContext>, VoidApplicationContext> {
+	public static final class HelloWorldApplication	extends Application<BackendWebClient<Object>, Object> {
 		
 		public static final String NAME = "Hello World Demo";
 		
@@ -27,15 +26,14 @@ public final class HelloWorldApplicationDemo {
 		}
 	}
 	
-	private static final class HelloWorldSession extends BackendGUIClientSession<VoidApplicationContext> {
+	public static final class HelloWorldSession extends BackendWebClientSession<Object> {
 		
 		@Override
 		protected void initialize() {
-			getRefGUI()
-			.pushLayerWithRootWidget(
-				new Label()
+			getRefGUI().pushLayerWithRootControl(
+				new Text()
 				.setText("Hello World")
-				.onStyle(l -> l.setTextSizeForState(ControlState.BASE, 50))
+				.editStyle(s -> s.setTextSizeForState(ControlState.BASE, 50))
 			);
 		}
 	}
